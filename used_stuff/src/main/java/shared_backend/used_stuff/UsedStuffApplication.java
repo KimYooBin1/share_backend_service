a0c62e7a5ad960da13480546eed2162c561cbf66
kimyoobin <55120730+KimYooBin1@users.noreply.github.com>
2024-02-20 20:51:08 +0900

    first commit

---
+++ used_stuff/src/main/java/shared_backend/used_stuff/UsedStuffApplication.java
@@ -0,0 +1,13 @@
+package shared_backend.used_stuff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsedStuffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsedStuffApplication.class, args);
+	}
+
+}
---
+++ used_stuff/src/test/java/shared_backend/used_stuff/UsedStuffApplicationTests.java
@@ -0,0 +1,13 @@
+package shared_backend.used_stuff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsedStuffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
