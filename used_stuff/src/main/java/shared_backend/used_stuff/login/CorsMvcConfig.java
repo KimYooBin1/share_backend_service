a1aed26f7ec6788b4de3a95e5cad93b7714e8ea5
kimyoobin <55120730+KimYooBin1@users.noreply.github.com>
2024-03-10 19:41:08 +0900

    cors 설정

---
+++ used_stuff/src/main/java/shared_backend/used_stuff/login/CorsMvcConfig.java
@@ -0,0 +1,17 @@
+package shared_backend.used_stuff.login;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Configuration
+@Slf4j
+public class CorsMvcConfig implements WebMvcConfigurer {
+	@Override
+	public void addCorsMappings(CorsRegistry corsRegistry) {
+		corsRegistry.addMapping("/**")
+			.allowedOrigins("http://localhost:3000");
+	}
+}
--- used_stuff/src/main/java/shared_backend/used_stuff/login/SecurityConfig.java
+++ used_stuff/src/main/java/shared_backend/used_stuff/login/SecurityConfig.java
@@ -1,5 +1,7 @@
 package shared_backend.used_stuff.login;
 
+import java.util.Collections;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -12,7 +14,10 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -38,6 +43,27 @@ public class SecurityConfig{
 
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http
+			.cors((corsCustomizer -> corsCustomizer.configurationSource(new CorsConfigurationSource() {
+
+				@Override
+				public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+
+					CorsConfiguration configuration = new CorsConfiguration();
+
+					configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+					configuration.setAllowedMethods(Collections.singletonList("*"));
+					configuration.setAllowCredentials(true);
+					configuration.setAllowedHeaders(Collections.singletonList("*"));
+					configuration.setMaxAge(3600L);
+
+					configuration.setExposedHeaders(Collections.singletonList("Authorization"));
+
+					return configuration;
+				}
+			})));
+
+
 		http
 			.csrf(AbstractHttpConfigurer::disable);
 
